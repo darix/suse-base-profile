6604b2451bb8b50fe3bd4c4c8be83bcb713a6465
Marcus Rückert <darix@nordisch.org>
2024-05-15 15:11:41 +0200

    make it work with the new layout

--- tools/salt-update-include.sh
+++ tools/salt-update-include.sh
@@ -2,10 +2,10 @@ for hostname in "$@" ; do
   echo "Handling host $hostname ... pinging first ... output following:"
   if salt "$hostname" test.ping ; then
     echo "Deploying new os-release file for rel $RELEASE on $hostname"
-    salt "$hostname" state.apply profile.base.os-update-$RELEASE
+    salt "$hostname" state.apply profile-base.os-update-$RELEASE
     echo "Clear all other caches and update data on minion $hostname ... output following:"
-    salt "$hostname" state.apply profile.base.sync-salt-information
+    salt "$hostname" state.apply profile-base.sync-salt-information
     echo "Deploy new repositories on $hostname"
-    salt "$hostname" state.apply profile.base.zypp
+    salt "$hostname" state.apply profile-base.zypp
   fi
 done
